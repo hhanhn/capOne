e48f2ad936b0acc7a46666c25ec566f1be4a42cc
hhanhn <h.hanhnguyen20@gmail.com>
2019-06-23 23:07:01 -0400

    Updates

---
+++ precache-manifest.76b1a6276296457574780be12360cf22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542ed33e7140475fa8604bc2351ea0d8",
+    "url": "/capOne/index.html"
+  },
+  {
+    "revision": "e87d34e5d144533cd7b7",
+    "url": "/capOne/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4f06a0b7cdd499329c8d",
+    "url": "/capOne/static/css/main.28cb142c.chunk.css"
+  },
+  {
+    "revision": "e87d34e5d144533cd7b7",
+    "url": "/capOne/static/js/2.829d80c5.chunk.js"
+  },
+  {
+    "revision": "4f06a0b7cdd499329c8d",
+    "url": "/capOne/static/js/main.6bc8aa65.chunk.js"
+  },
+  {
+    "revision": "d60a974ab6db8825201e",
+    "url": "/capOne/static/js/runtime~main.71065d02.js"
+  },
+  {
+    "revision": "e1c2a054bc0b593e4404c978766933a6",
+    "url": "/capOne/static/media/bg.e1c2a054.jpg"
+  }
+]);
